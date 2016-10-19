33969862e3fadadb56a38353f1ba0b679247255b
Guy Daher <guydaher1@gmail.com>
2016-10-18 23:48:39 -0700

    PodFile now referencing GitHub repo for getting the GDImage module

---
+++ Demo/Pods/GDImage/GDImage/Guy.swift
@@ -0,0 +1,17 @@
+//
+//  Guy.swift
+//  GDImage
+//
+//  Created by Guy Daher on 10/18/16.
+//  Copyright © 2016 guydaher. All rights reserved.
+//
+
+import Foundation
+
+class Guy{
+    init(){}
+    
+    public func sum(a:Int, b:Int) -> Int{
+        return a + b
+    }
+}
