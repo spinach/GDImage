0b5403e76aba41882966f8149aae027d80e7bfd1
Guy Daher <guydaher1@gmail.com>
2016-10-18 22:11:36 -0700

    Initial Commit

---
+++ GDImageTests/GDImageTests.swift
@@ -0,0 +1,38 @@
+//
+//  GDImageTests.swift
+//  GDImageTests
+//
+//  Created by Guy Daher on 10/18/16.
+//  Copyright © 2016 guydaher. All rights reserved.
+//
+
+import XCTest
+@testable import GDImage
+
+class GDImageTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let guy = Guy()
+        XCTAssertTrue(guy.sum(a: 2, b: 3) == 5)
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
