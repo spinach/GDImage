fbb80d3ef17eb7da59462367937fa4940d965a27
Guy Daher <guydaher1@gmail.com>
2016-10-20 19:27:55 -0700

    Tryout download of image using URLSession.shared.dataTask

--- Demo/Demo/ViewController.swift
+++ Demo/Demo/ViewController.swift
@@ -10,20 +10,44 @@ import UIKit
 import GDImage
 
 class ViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let guy = Guy()
         print(guy.sum(a: 3, b: 5))
+        
+        print("Begin of code")
+        if let checkedUrl = URL(string: "https://s3.amazonaws.com/dummy-images-guy/algolia-logo.jpg") {
+            imageView.contentMode = .scaleAspectFit
+            downloadImage(url: checkedUrl)
+        }
+        print("End of code. The image will continue downloading in the background and it will be loaded when it ends.")
+
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
+    func getDataFromUrl(url: URL, completion: @escaping (_ data: Data?, _  response: URLResponse?, _ error: Error?) -> Void) {
+        URLSession.shared.dataTask(with: url) {
+            (data, response, error) in
+            completion(data, response, error)
+            }.resume()
+    }
+    
+    func downloadImage(url: URL) {
+        print("Download Started")
+        getDataFromUrl(url: url) { (data, response, error)  in
+            guard let data = data, error == nil else { return }
+            print(response?.suggestedFilename ?? url.lastPathComponent)
+            print("Download Finished")
+            DispatchQueue.main.async() { () -> Void in
+                self.imageView.image = UIImage(data: data)
+            }
+        }
+    }
 
 }
 
