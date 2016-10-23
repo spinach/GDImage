77813473e4504733107c1ec9a418392af784c200
Guy Daher <guydaher1@gmail.com>
2016-10-23 16:28:04 -0700

    Prototype for downloading async an image via UIImageView extension. Tested in Demo and it works.

--- Demo/Demo/ViewController.swift
+++ Demo/Demo/ViewController.swift
@@ -14,40 +14,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let guy = Guy()
-        print(guy.sum(a: 3, b: 5))
-        
-        print("Begin of code")
-        if let checkedUrl = URL(string: "https://s3.amazonaws.com/dummy-images-guy/algolia-logo.jpg") {
-            imageView.contentMode = .scaleAspectFit
-            downloadImage(url: checkedUrl)
-        }
-        print("End of code. The image will continue downloading in the background and it will be loaded when it ends.")
-
-        
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-
-    func getDataFromUrl(url: URL, completion: @escaping (_ data: Data?, _  response: URLResponse?, _ error: Error?) -> Void) {
-        URLSession.shared.dataTask(with: url) {
-            (data, response, error) in
-            completion(data, response, error)
-            }.resume()
-    }
-    
-    func downloadImage(url: URL) {
-        print("Download Started")
-        getDataFromUrl(url: url) { (data, response, error)  in
-            guard let data = data, error == nil else { return }
-            print(response?.suggestedFilename ?? url.lastPathComponent)
-            print("Download Finished")
-            DispatchQueue.main.async() { () -> Void in
-                self.imageView.image = UIImage(data: data)
-            }
+                
+        if let url = URL(string: "https://s3.amazonaws.com/dummy-images-guy/algolia-logo.jpg") {
+            self.imageView.setImage(withUrl: url)
         }
     }
-
 }
 
--- GDImage/Guy.swift
+++ GDImage/Guy.swift
@@ -8,10 +8,23 @@
 
 import Foundation
 
-public class Guy{
-    public init(){}
-    
-    public func sum(a:Int, b:Int) -> Int{
-        return a + b
+extension UIImageView {
+    public func setImage(withUrl url: URL) {
+        contentMode = .scaleAspectFit
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard
+                let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
+                let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
+                let data = data, error == nil,
+                let image = UIImage(data: data)
+                else { return }
+            DispatchQueue.main.async() { () -> Void in
+                self.image = image
+            }
+        }.resume()
+    }
+    public func setImage(withLink link: String) {
+        guard let url = URL(string: link) else { return }
+        setImage(withUrl: url)
     }
 }
